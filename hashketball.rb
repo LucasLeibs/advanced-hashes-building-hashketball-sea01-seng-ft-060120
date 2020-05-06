ff8a73fb67f7545f9ce0fd4f970861cb8492601c
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-05-06 18:33:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,37 @@
-# Write your code here!
+def game_hash
+  game = {
+    :home => {
+      :team_name => {},
+      :colors => [],
+      :players => [
+        {:player_name => "Alan Jefferson",
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3, 
+        :blocks => 1,
+        :slam_dunks => 1,
+        },
+      ],
+    },
+    :away => {
+      :team_name => {},
+      :colors => [],
+      :players => [
+        :player_name => "Jeff Adrien",
+        :number => {},
+        :shoe => {},
+        :points => {},
+        :rebounds => {},
+        :assists => {},
+        :steals => {},
+        :blocks => {},
+        :slam_dunks => {},
+        ],
+    },
+    
+    
+  }
+end
