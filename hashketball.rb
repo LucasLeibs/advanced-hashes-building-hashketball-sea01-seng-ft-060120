c6b5b8793b537307116cf8310d3b62a9ddff36f0
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-05-06 18:38:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -30,7 +30,7 @@ def game_hash
       :team_name => {},
       :colors => [],
       :players => [
-        {:player_name =>,
+        {:player_name => "Jeff Adrien",
         :number => ,
         :shoe => ,
         :points => ,
